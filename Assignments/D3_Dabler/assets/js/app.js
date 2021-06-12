6afc1f3213f60831259dea72d29706708b3d28ed
leila Zintsem a moute <leilazintsemamoute@gmail.com>
2021-06-12 18:54:03 -0500

    setup SVG

--- Assignments/D3_Dabler/assets/js/app.js
+++ Assignments/D3_Dabler/assets/js/app.js
@@ -84,34 +84,34 @@ function xScale(data, chosenXAxis) {
 
 
 
+// Let's setup SVG
 
-// //########################  3.  SVG Setup ###################################//
 
-// var svgWidth = 960;
-// var svgHeight = 500;
+var svgWidth = 960;
+var svgHeight = 500;
 
-// var margin = {
-//   top: 20,
-//   right: 40,
-//   bottom: 80,
-//   left: 100
-// };
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 80,
+  left: 100
+};
 
-// // xScale uses width so xScale() can only be called below this point
-// var width = svgWidth - margin.left - margin.right;
-// var height = svgHeight - margin.top - margin.bottom;
+// xScale uses width so xScale() can only be called below this point
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
 
-// // Create an SVG wrapper, append an SVG group that will hold our chart,
-// // and shift the latter by left and top margins.
-// var svg = d3
-//   .select(".chart")
-//   .append("svg")
-//   .attr("width", svgWidth)
-//   .attr("height", svgHeight);
+// Create an SVG wrapper, append an SVG group that will hold our chart,
+// and shift the latter by left and top margins.
+var svg = d3
+  .select(".chart")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
 
-// // Append an SVG group
-// var chartGroup = svg.append("g")
-//   .attr("transform", `translate(${margin.left}, ${margin.top})`);
+// Append an SVG group
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 
 
